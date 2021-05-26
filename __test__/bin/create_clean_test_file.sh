7b64ccb793698f7f964edbae214a47faf882e44c
Florian Friedrich <ffried@me.com>
2021-05-26 15:43:31 +0200

    Attempt to add linux support

--- __test__/bin/create_clean_test_file.sh
+++ __test__/bin/create_clean_test_file.sh
@@ -3,10 +3,10 @@
 set -euxo pipefail
 
 if [ $# -ne 1 ]; then
-	printf "Invalid argument count!\n"
-	printf "Usage: $0 OUTPUT_FOLDER\n"
+	printf '%s' "Invalid argument count!\n"
+	printf '%s' "Usage: $0 OUTPUT_FOLDER\n"
 	exit 1
 fi
 
 mkdir -p "${1}"
-printf "test-content" > "${1}/_test_file.txt"
+echo "test-content" > "${1}/_test_file.txt"
